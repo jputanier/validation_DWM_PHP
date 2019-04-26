25183805239d371615a63e35f0e3dc6fb061a271
jputanier <j.putanier@gmail.com>
2019-04-26 12:19:09 +0200

    MAJ css

--- index.php
+++ index.php
@@ -13,34 +13,36 @@ session_start();
 </head>
 <body>
     <?php require_once './templates/header.php' ?>
-    <?php if(isset($_SESSION['pseudo'], $_SESSION['mdp'])): ?>
-        <?php if($_SESSION['pseudo'] == "login" && $_SESSION['mdp'] == "zero"): ?>
-        <h1>Choisissez ce que vous souhaitez afficher sur la page:</h1>
-        <form method="POST" action="./controllers/formulaire.php" >
-            <label for="lorem">Un lorem:</label>
-            <input type="checkbox" name="lorem" id="lorem">
+    <main>
+        <?php if(isset($_SESSION['pseudo'], $_SESSION['mdp'])): ?>
+            <?php if($_SESSION['pseudo'] == "login" && $_SESSION['mdp'] == "zero"): ?>
+            <h1>Choisissez ce que vous souhaitez afficher sur la page:</h1>
+            <form method="POST" action="./controllers/formulaire.php" >
+                <label for="lorem">Un lorem:</label>
+                <input type="checkbox" name="lorem" id="lorem">
 
-            <label for="img1"><img src="">Une icone</label>
-            <input type="checkbox" name="img1" id="img1">
+                <label for="img1"><img src="">Une icone</label>
+                <input type="checkbox" name="img1" id="img1">
 
-            <label for="img2"><img src="">Une photo</label>
-            <input type="checkbox" name="img2" id="img2">
+                <label for="img2"><img src="">Une photo</label>
+                <input type="checkbox" name="img2" id="img2">
 
-            <input type="submit" id="envoyer" value="Envoyer">
-        </form>
-            <?php if(isset($_SESSION['lorem'])): ?>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatum temporibus, laborum maxime saepe quisquam ratione harum expedita sit, labore facilis doloremque nostrum deserunt! Esse dolor laudantium corporis ex facilis.</p>
+                <input type="submit" id="envoyer" value="Envoyer">
+            </form>
+                <?php if(isset($_SESSION['lorem'])): ?>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi voluptatum temporibus, laborum maxime saepe quisquam ratione harum expedita sit, labore facilis doloremque nostrum deserunt! Esse dolor laudantium corporis ex facilis.</p>
+                <?php endif ?>
+                <?php if(isset($_SESSION['img1'])): ?>
+                    <img src="./img/icone.jpg">
+                <?php endif ?>
+                <?php if(isset($_SESSION['img2'])): ?>
+                    <img src="./img/photo.jpg">
+                <?php endif ?>
+            <?php else: ?>
+            <p>Mot de passe incorrect</p>
             <?php endif ?>
-            <?php if(isset($_SESSION['img1'])): ?>
-                <img src="./img/icone.jpg">
-            <?php endif ?>
-            <?php if(isset($_SESSION['img2'])): ?>
-                <img src="./img/photo.jpg">
-            <?php endif ?>
-        <?php else: ?>
-        <p>Mot de passe incorrect</p>
         <?php endif ?>
-    <?php endif ?>
+    </main>
     <?php require_once './templates/footer.html' ?>
 </body>
 </html>
--- public/connecter.php
+++ public/connecter.php
@@ -12,16 +12,17 @@ session_start();
 </head>
 <body>
     <?php require_once '../templates/header.php' ?>
+    <main>
+        <form method="POST" action="../controllers/form.php">
+            <label for="pseudo">Pseudo:</label>
+            <input type="text" name="pseudo" id="pseudo" required>
 
-    <form method="POST" action="../controllers/form.php">
-        <label for="pseudo">Pseudo:</label>
-        <input type="text" name="pseudo" id="pseudo" required>
-
-        <label for="mdp">Mot de passe:</label>
-        <input type="password" name="mdp" id="mdp" required>
-        
-        <input type="submit" value="Valider" id="valider">
-    </form>
+            <label for="mdp">Mot de passe:</label>
+            <input type="password" name="mdp" id="mdp" required>
+            
+            <input type="submit" value="Valider" id="valider">
+        </form>
+    </main>
 
     <?php require_once '../templates/footer.html' ?>
 </body>
